8b163d7f9b96db1fb3032de9f7aab9b0e1209b40
Kiran413959 <kiranrajendran6@gmail.com>
2024-07-04 22:28:14 +0530

    backend started

---
+++ routes/Auth_Routes.js
@@ -0,0 +1,47 @@
+const express =require('express')
+const router = express.Router()
+
+
+const{
+    signupget,
+    signuppost,
+    loginget,
+    loginpost,
+    homeget,
+    resume_selectionget,
+    resume_selectionpost,
+    experienceget,
+    experiencepost,
+    QandAget,
+    QandApost,
+}=require('../controllers/Auth_Controller')
+
+
+router
+
+    .get('/signup',signupget)
+
+    .post('/signup',signuppost)
+
+    .get('/login',loginget)
+
+    .post('login',loginpost)
+
+    .get('/home',homeget)
+
+    .get('/resume_selection',resume_selectionget)
+
+    .post('/resume_selection',resume_selectionpost)
+
+    .get('/experience',experienceget)
+
+    .post('/experience',experiencepost )
+
+    .get('/Q&A',QandAget)
+
+    .post('/Q&A',QandApost)
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.port || 4201
+const path = require('path')
+const env = require('dotenv')
+const cors= require('cors')
+env.config()
+
+app.use(cors())
+mongoose.connect(process.env.DB_LINK).then(()=>{
+    console.log("DB is runing");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+
+const router = require('./route')
+
+app.use(express.static('Public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+app.use( '', router)
+
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
